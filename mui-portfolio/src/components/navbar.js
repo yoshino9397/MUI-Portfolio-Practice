4a709238319d54fd9fddc4b76ae7ace6dc0c9fb6
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-03-02 00:20:23 -0800

    footer start

---
+++ mui-portfolio/src/components/navbar.js
@@ -0,0 +1,36 @@
+// import React from "react";
+// import Box from "@material-ui/core/Box";
+// import AppBar from '@mui/material/AppBar'
+// import Toolbar from '@mui/material/Toolbar';
+// import IconButton from '@mui/material/IconButton';
+// import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
+// import Typography from "@mui/material/Typography";
+// import Drawer from '@mui/material/Drawer';
+
+
+
+// const Navbar = () => {
+//   return (
+//  <>
+//  <Box component="nav">
+//  <AppBar position="static">
+//    <Toolbar>
+//      <IconButton onClick={() => setOpen(true)}>
+//        <KeyboardDoubleArrowLeftIcon />
+//      </IconButton>
+//      <Typography variant="h5" >
+//        Portfolio
+//      </Typography>
+//    </Toolbar>
+//  </AppBar>
+// </Box>
+// <Drawer open={open} anchor="right" onClose={() => setOpen(false)}>
+//  {sideList()}
+//  <Footer />
+// </Drawer>
+
+// </>
+//   )  
+// };
+
+// export default Navbar;
---
+++ try-file/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import Footer from "./components/Footer";
+
+function App() {
+  return <Footer />;
+}
+
+export default App;
