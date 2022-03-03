c0929c5ea1efd877d0daa7defe0798ee03ccf112
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-03-02 22:33:06 -0800

    anuway

--- mui-portfolio/src/components/navbar.js
+++ mui-portfolio/src/components/navbar.js
@@ -1,36 +1,119 @@
-// import React from "react";
-// import Box from "@material-ui/core/Box";
-// import AppBar from '@mui/material/AppBar'
-// import Toolbar from '@mui/material/Toolbar';
-// import IconButton from '@mui/material/IconButton';
-// import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
-// import Typography from "@mui/material/Typography";
-// import Drawer from '@mui/material/Drawer';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import Typography from "@mui/material/Typography";
+import Drawer from "@mui/material/Drawer";
+import Avatar from "@mui/material/Avatar";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HomeIcon from "@mui/icons-material/Home";
+import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
+import AppsIcon from "@mui/icons-material/Apps";
+import ContactMailIcon from "@mui/icons-material/ContactMail";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { orange } from "@mui/material/colors";
+import { red } from "@mui/material/colors";
+import { blue } from "@mui/material/colors";
 
+import avatar from "../images/avatar.jpg";
+import Footer from "../components/Footer";
 
+const menuItems = [
+  { listIcon: <HomeIcon />, listText: "Home", listPath: "/" },
+  { listIcon: <AssignmentIndIcon />, listText: "Resume", listPath: "/resume" },
+  { listIcon: <AppsIcon />, listText: "Portfolio", listPath: "/portfolio" },
+  { listIcon: <ContactMailIcon />, listText: "Contact", listPath: "/contact" },
+];
 
-// const Navbar = () => {
-//   return (
-//  <>
-//  <Box component="nav">
-//  <AppBar position="static">
-//    <Toolbar>
-//      <IconButton onClick={() => setOpen(true)}>
-//        <KeyboardDoubleArrowLeftIcon />
-//      </IconButton>
-//      <Typography variant="h5" >
-//        Portfolio
-//      </Typography>
-//    </Toolbar>
-//  </AppBar>
-// </Box>
-// <Drawer open={open} anchor="right" onClose={() => setOpen(false)}>
-//  {sideList()}
-//  <Footer />
-// </Drawer>
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: orange[300],
+    },
+    secondary: {
+      main: red[800],
+    },
+  },
+});
 
-// </>
-//   )  
-// };
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
 
-// export default Navbar;
+  const sideList = () => (
+    <ThemeProvider theme={theme}>
+      <Box
+        component="div"
+        sx={{
+          width: 250,
+          background: "#511",
+          height: "100%",
+        }}
+      >
+        <Avatar
+          src={avatar}
+          alt="Yoshino"
+          sx={{
+            display: "block",
+            margin: "2rem auto",
+            width: 120,
+            height: 120,
+          }}
+        />
+        <Divider />
+        <List>
+          {menuItems.map((item, i) => (
+            <ListItem
+              button
+              key={i}
+              onClick={() => setOpen(false)}
+              component={Link}
+              to={item.listPath}
+              color="primary"
+            >
+              <ListItemIcon color="primary">{item.listIcon}</ListItemIcon>
+              <ListItemText primary={item.listText} />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </ThemeProvider>
+  );
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Box component="nav">
+          <AppBar
+            position="absolute"
+            color="primary"
+            sx={{
+              margin: 0,
+            }}
+          >
+            <Toolbar>
+              <IconButton onClick={() => setOpen(true)}>
+                <KeyboardDoubleArrowLeftIcon color="secondary" />
+              </IconButton>
+              <Typography variant="h5" color="#8a1c1c">
+                Portfolio
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <Drawer open={open} anchor="left" onClose={() => setOpen(false)} >
+          {sideList()}
+          <Footer />
+        </Drawer>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default Navbar;
